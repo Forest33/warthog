15f8b5b280718946d104ad2047eb4dc7deaca45a
Anton Boykevich <ab@impaya.io>
2022-11-13 21:38:26 +0300

    database migration fix

--- deploy/app/migrations/1633685677_init.up.sql
+++ deploy/app/migrations/1633685677_init.up.sql
@@ -1,17 +1,17 @@
-CREATE TABLE gui_config
+CREATE TABLE IF NOT EXISTS gui_config
 (
-    window_width  INT NOT NULL       DEFAULT 1024,
-    window_height INT NOT NULL       DEFAULT 768,
-    window_x      INT NOT NULL       DEFAULT 50,
-    window_y      INT NOT NULL       DEFAULT 50,
-    created_at    DATETIME DEFAULT (datetime('now', 'localtime')) NOT NULL,
-    updated_at    DATETIME DEFAULT (datetime('now', 'localtime')) NOT NULL
+    window_width  INT NOT NULL DEFAULT 1024,
+    window_height INT NOT NULL DEFAULT 768,
+    window_x      INT NOT NULL DEFAULT 50,
+    window_y      INT NOT NULL DEFAULT 50,
+    created_at    DATETIME     DEFAULT (datetime('now', 'localtime')) NOT NULL,
+    updated_at    DATETIME     DEFAULT (datetime('now', 'localtime')) NOT NULL
 );
 
 INSERT INTO gui_config (window_width, window_height, window_x, window_y)
 VALUES (1024, 768, 50, 50);
 
-CREATE TABLE workspace
+CREATE TABLE IF NOT EXISTS workspace
 (
     id         INTEGER PRIMARY KEY,
     parent_id  INTEGER                              NULL,
@@ -21,6 +21,6 @@ CREATE TABLE workspace
     data       TEXT                                 NULL,
     sort       INTEGER                              NOT NULL DEFAULT 0,
     expanded   BOOL                                 NOT NULL DEFAULT FALSE,
-    created_at TIMESTAMP DATETIME                            DEFAULT (datetime('now', 'localtime')) NOT NULL,
-    updated_at TIMESTAMP DATETIME                            DEFAULT (datetime('now', 'localtime')) NOT NULL
+    created_at DATETIME                                      DEFAULT (datetime('now', 'localtime')) NOT NULL,
+    updated_at DATETIME                                      DEFAULT (datetime('now', 'localtime')) NOT NULL
 );
